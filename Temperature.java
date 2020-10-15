85999d90ea82f8d71f3383722fefdb8a71704b8b
Hdoyle09 <2170050@lfschools.net>
2020-10-15 18:04:37 +0000

    temp

--- Temperature.java
+++ Temperature.java
@@ -5,6 +5,7 @@ public class Temperature
      */
     public double convertTemperature(double celsius)
     {
-    fahrenheit=9.0/5.0 * celsius + 32.0;
+    double fahrenheit=9.0/5.0 * celsius + 32.0;
+    return fahrenheit;
     }
 }
